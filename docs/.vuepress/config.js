e8688a596123177bb56e04e3725d45eb40830726
webberg <webbery@foxmail.com>
2021-04-12 22:55:31 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,65 @@
+module.exports = {
+  title: "Civet",
+  description: "Civet是一款开源管理软件。它的目标是提供各种资源的管理，使快速查找需要的数据和信息成为可能",
+  base: '/civet/',
+  theme: 'thindark',
+  themeConfig : {
+    nav : [
+      { text: '介绍', link: '/introduction' },
+      { text: '整体框架', link: '/frame' },
+      { text: '前端', link: '/frontend' },
+      { text: '数据库', link: '/db' },
+      { text: '扩展及能力', link: '/extension' },
+      {
+        text: 'GitHub', link: 'https://github.com/webbery/civet'
+      }
+    ],
+    sidebar: {
+      '/frontend' : [
+        {
+          title: '属性面板',
+          children: ['标题属性', '列表属性', '画板属性', '特殊属性']
+        },
+        {
+          title: '导航面板',
+          children: ['三导航', '分类导航']
+        },
+        {
+          title: '主面板',
+        },
+        {
+          title: '展示面板',
+        }
+      ],
+      '/db' : [
+        { title: '查询' }, { title: '添加/修改/删除' }
+      ],
+      '/extension' : [
+        {
+          title: '浏览器扩展',
+          children: ['数据交互']
+        },
+        {
+          title: '数据源扩展',
+          children: ['']
+        },
+        {
+          title: '存储扩展',
+          children: ['']
+        },
+        {
+          title: '算法扩展',
+          children: ['']
+        },
+        {
+          title: 'UI扩展',
+          children: ['']
+        },
+        {
+          title: '常用扩展集',
+          children: ['']
+        }
+      ]
+    }
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+---
+home: true
+actionText: 下载
+actionLink: https://hub.fastgit.org/webbery/civet/releases
+features:
+- title: 开源
+  details: 开源的civet可以一直维护下去，不会因为各种原因停止前进的脚步。
+- title: 可扩展
+  details: 多样化的扩展支持，浏览器扩展、文件支持扩展、算法扩展，甚至对代码仓库分支/服务器管理的扩展，都能通过扩展API实现。
+- title: 高性能
+  details: 本地数据库采用共享内存作为存储引擎，并使用C++开发的查询引擎，提升了性能和易用性。
+footer: MIT Licensed | Copyright © 2021-present Evan You
+---
